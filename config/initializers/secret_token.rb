d484734217b4721c4d56c5995a5ebb9ebb00b2cf
Ryan Bigg <radarlistener@gmail.com>
2013-12-09 09:52:06 +1100

    Add bare Rails 3.2.16 application

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Spree::Application.config.secret_token = 'b83561b04d1885e820b61be9e9405b2c9f00cbf0b9bc5d39b7c3a189b635d8e1c9e378dea65d15b57182e74eaddfdca31348bcf01cb6b280e790324b01a5c46d'
