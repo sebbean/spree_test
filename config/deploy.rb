4fcf7a539f340f38598dc179315ac31d46345346
Ryan Bigg <radarlistener@gmail.com>
2013-12-19 16:58:33 +1100

    Use ansible-rails-app deploy.rb

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,46 +1,67 @@
 set :application, 'spree'
-set :repo_url, 'git://github.com/radar/spree_test'
+set :repo_url, 'git://github.com/radar/nf_performance_spree'
 set :deploy_to, '/data/spree'
 set :log_level, :debug
 set :linked_files, %w{config/database.yml}
 set :linked_dirs, %w{tmp/sockets log config/puma public/spree}
 set :sockets_path, Pathname.new("#{fetch(:deploy_to)}/shared/tmp/sockets/")
-set :environment, "production"
 
-# ask :branch, proc { `git rev-parse --abbrev-ref HEAD`.chomp }
+# These puma settings are only here because capistrano-puma is borked.
+# See issue #4.
+set :puma_roles, :app
+set :puma_socket, "unix://#{fetch(:sockets_path).join('puma_' + fetch(:application) + '.sock')}"
+set :pumactl_socket, "unix://#{fetch(:sockets_path).join('pumactl_' + fetch(:application) + '.sock')}"
+set :puma_state, fetch(:sockets_path).join('puma.state') 
+set :puma_log, -> { shared_path.join("log/puma-#{fetch(:stage )}.log") }
+set :puma_flags, nil 
 
-# set :deploy_to, '/var/www/my_app'
-# set :scm, :git
-
-# set :format, :pretty
-# set :log_level, :debug
-# set :pty, true
-
-# set :linked_files, %w{config/database.yml}
-# set :linked_dirs, %w{bin log tmp/pids tmp/cache tmp/sockets vendor/bundle public/system}
-
-# set :default_env, { path: "/opt/ruby/bin:$PATH" }
-# set :keep_releases, 5
+set :bundle_flags, '--deployment'
 
 namespace :deploy do
-
-  desc 'Restart application'
   task :restart do
-    on roles(:app), in: :sequence, wait: 5 do
-      # Your restart mechanism here, for example:
-      # execute :touch, release_path.join('tmp/restart.txt')
+    invoke 'puma:restart'
+  end
+end
+
+namespace :spree_sample do
+  task :load do
+    on roles(:app) do
+      within release_path do
+        ask(:confirm, "Are you sure you want to delete everything and start again? Type 'yes'")
+        if fetch(:confirm) == "yes"
+          execute :rake, "db:reset AUTO_ACCEPT=true"
+          execute :rake, "spree_sample:load"
+        end
+      end
+    end
+  end
+end
+
+namespace :puma do
+  desc "Restart puma instance for this application"
+  task :restart do
+    on roles fetch(:puma_roles) do
+      within release_path do
+        execute :bundle, "exec pumactl -S #{fetch(:puma_state)} restart"
+      end
     end
   end
 
-  after :restart, :clear_cache do
-    on roles(:web), in: :groups, limit: 3, wait: 10 do
-      # Here we can do anything such as:
-      # within release_path do
-      #   execute :rake, 'cache:clear'
-      # end
+  desc "Show status of puma for this application"
+  task :status do
+    on roles fetch(:puma_roles) do
+      within release_path do
+        execute :bundle, "exec pumactl -S #{fetch(:puma_state)} stats"
+      end
     end
   end
 
-  after :finishing, 'deploy:cleanup'
-
+  desc "Show status of puma for all applications"
+  task :overview do
+    on roles fetch(:puma_roles) do
+      within release_path do
+        execute :bundle, "exec puma status"
+      end
+    end
+  end
 end
