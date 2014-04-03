69226b625ef3585e28c4f4c7ac7b185c2f8b6fc0
Ryan Bigg <radarlistener@gmail.com>
2014-04-04 08:41:02 +1100

    Update schema

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140324232727) do
+ActiveRecord::Schema.define(version: 20140403052430) do
 
   create_table "spree_activators", force: true do |t|
     t.string   "description"
